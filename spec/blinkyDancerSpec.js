8d729a3bc5d6106fdb1a5d78a688d4a452b5a8dc
Alexei Sorokine <2picae@gmail.com>
2019-01-09 21:31:03 -0800

    Set up test suit for breakDancer and pulpDancer

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -33,3 +33,77 @@ describe('blinkyDancer', function() {
     });
   });
 });
+
+describe('breakDancer', function() {
+
+  var breakDancer, clock;
+  var timeBetweenSteps = 200;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    breakDancer = new makeBreakDancer(20, 30, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(breakDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that makes its node blink', function() {
+    sinon.spy(breakDancer.$node, 'toggle');
+    breakDancer.step();
+    expect(breakDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(breakDancer, 'step');
+      console.log(clock);
+      expect(breakDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(breakDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(breakDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+describe('pulpDancer', function() {
+
+  var pulpDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    pulpDancer = new makepulpDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(pulpDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that makes its node blink', function() {
+    sinon.spy(pulpDancer.$node, 'toggle');
+    pulpDancer.step();
+    expect(pulpDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(pulpDancer, 'step');
+      console.log(clock);
+      expect(pulpDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(pulpDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(pulpDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+
